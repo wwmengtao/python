3eaf12083ead6cd34f4663df1b429b5bc1b2068d
wwmengtao <wwmengtao@163.com>
2020-06-13 20:07:23 +0800

    提交
    Signed-off-by: wwmengtao <wwmengtao@163.com>

--- getWebInfo/opeExcel.py
+++ getWebInfo/opeExcel.py
@@ -3,12 +3,76 @@ import os
 import xlrd
 import xlwt
 from datetime import date,datetime
- 
+from pathlib import Path
+from xlutils.copy import copy
+
+# 函数判断
+def create_excel_sheet(fileName, sheetName):
+  fileExist = False
+  sheetExist = False
+  global wb_xlrd
+  global wb_xlwt
+  global sheetRows
+  # 1.首先判断文件是否存在
+  try:
+    wb_xlrd = xlrd.open_workbook(fileName)  # 打开工作簿
+    fileExist = True
+    wb_xlwt = copy(wb_xlrd)# 将xlrd对象拷贝转化为xlwt对象
+  except Exception:
+    print('文件不存在：'+fileName)
+    wb_xlwt = xlwt.Workbook()  # 新建一个工作簿
+  # 2.判断文件中对应的sheet是否存在
+  try:
+    st_xlrd = wb_xlrd.sheet_by_name(sheetName)  # 获取工作簿中名称为sheetName的表格
+    sheetExist = True
+    sheetRows = st_xlrd.nrows  # 获取表格中已存在的数据的行数
+  except Exception:
+    print('sheet不存在：'+sheetName)
+    wb_xlwt.add_sheet(sheetName)  # 在工作簿中新建一个表格
+    sheetRows = 0
+  # 3.文件或者sheet不存在，那么保存
+  if fileExist == False or sheetExist == False:
+    wb_xlwt.save(fileName)
+  return sheetRows
+
+#获取excel中指定sheet名称的索引
+def get_sheet_index(wb_xlrd, gSheetName):
+  sheetIndex = -1;
+  for i in range(0, len(wb_xlrd.sheet_names())):
+    sheetName = wb_xlrd.sheet_names()[i]
+    # print(sheetName)
+    if sheetName==gSheetName:
+      sheetIndex = i
+      break;
+  return sheetIndex
+
+def write_sheet_value(st_xlwt, rows_rd, value):
+  Length = len(value)  # 获取需要写入数据的行数
+  for i in range(0, Length):
+    for j in range(0, len(value[i])):
+      st_xlwt.write(i + rows_rd, j, value[i][j])  # 追加写入数据，注意是从i+rows_old行开始写入
+
+ # path指的是excel文件的路径;value指的是数据,是一个嵌套列表
+def write_excel_xls_append(path, sheetName, value):
+    wb_xlrd = xlrd.open_workbook(path)  # 打开工作簿
+    st_xlrd = wb_xlrd.sheet_by_name(sheetName)  # 获取工作簿中名称为sheetName的表格
+    rows_rd = st_xlrd.nrows  # 获取表格中已存在的数据的行数
+    wb_xlwt = copy(wb_xlrd)  # 将xlrd对象拷贝转化为xlwt对象
+    sheetIndex = get_sheet_index(wb_xlrd, sheetName)
+    st_xlwt = wb_xlwt.get_sheet(sheetIndex) 
+    write_sheet_value(st_xlwt, rows_rd, value)
+    wb_xlwt.save(path)  # 保存工作簿
+
 def read_excel():
   # 打开文件
   filename = os.getcwd()+"\\test.xls"#\\t是转义，相当于\t
-  print(filename)
-  workbook = xlrd.open_workbook(filename)
+  # my_file = Path("/path/to/file")
+  if os.path.exists(filename):
+    print("存在："+filename)
+  else:
+    print("不存在："+filename)
+    return
+  workbook = xlrd.open_workbook(filename)#如果文件不存在则报错
   # 获取所有sheet
   # print workbook.sheet_names() #[u'sheet1', u'sheet2']
   sheet2_name = workbook.sheet_names()[1]
@@ -33,6 +97,30 @@ def read_excel():
    
   # 获取单元格内容的数据类型
   print(sheet2.cell(1,0).ctype)
- 
-if __name__ == '__main__':
+
+  #1. 获取指定sheet名称的sheet在excel文件中的索引 
+  sheetIndex = get_sheet_index(workbook, 'sheet3')
+  # print("sheet索引: "+str(sheetIndex))
+  if sheetIndex < 0:
+    print("not index")
+
+  wb_xlrd = xlrd.open_workbook(filename)  # 打开工作簿
+  wb_xlwt = copy(wb_xlrd)  # 将xlrd对象拷贝转化为xlwt对象
+  # try:
+  #   st_xlwt = wb_xlwt.get_sheet('sheet5')  # 获取转化后工作簿中的第一个表格
+  # except Exception:
+  #   print('异常说明')
+  #   wb_xlwt.add_sheet('sheet5')
+  #   wb_xlwt.save(filename)
+  try:
+    wb_xlrd.sheet_by_name(sheetName)
+  except Exception:
+    print('异常说明')
+  
+  # write_excel_xls(file_name, author_name, [["文章类型", "文章标题", "文章链接", "发表日期", "阅读数", "评论数"], ])
+
+
+def main_func():
   read_excel()
+if __name__ == '__main__':
+  main_func()  
